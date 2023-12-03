d0ce2fa7fdb4e397a2db7649ca3e6d87b79bf753
Diego Umbelino <54283429+diegout@users.noreply.github.com>
2023-12-03 00:43:54 -0300

    Update Trakt_film_api.js
    update api trakt js

--- Trakt_film_api.js
+++ Trakt_film_api.js
@@ -2,7 +2,7 @@ fetch('https://api.trakt.tv/movies/watched/daily', {
     headers: {
         'Content-Type': 'application/json',
         'trakt-api-version': '2',
-        'trakt-api-key': 'https://staging.trakt.tv/oauth/applications/2006'
+        'trakt-api-key': '6cc206bf89b66802c80c58d2cc1250e0d805e225fbcdab067c6c96a4da3c4454'
     }
 })
 .then(response => response.json())
